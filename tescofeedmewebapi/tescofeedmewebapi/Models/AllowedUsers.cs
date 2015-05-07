57c971b4f5f56639f4a7b272dd19fc66c1f1a0b2
Aysha Witcombe <aysha.witcombe@uk.tesco.com>
2015-05-07 22:14:42 +0100

    Added Chris's clubcard

--- tescofeedmewebapi/tescofeedmewebapi/Models/AllowedUsers.cs
+++ tescofeedmewebapi/tescofeedmewebapi/Models/AllowedUsers.cs
@@ -2,8 +2,8 @@
 {
     static public class AllowedUsers
     {
-        public const string User1 = "polly";
-        public const string User2 = "nick";
+        public const string User1 = "634004024189823703";
+        public const string User2 = "polly";
         public const string User3 = "defaultUser";
     }
 }
